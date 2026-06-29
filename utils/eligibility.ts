b479f9deff62bf37b7a7695bb9bba6c629bec1a0
therassistantllc <admin@therassistant.com>
2026-06-29 11:10:29 -0600

    Add eligibility utilities

---
+++ utils/eligibility.ts
@@ -0,0 +1,6 @@
+export const formatMemberId = (...args: unknown[]) => args[0];
+export const normalizeMemberId = (...args: unknown[]) => args[0];
+export const formatGroupNumber = (...args: unknown[]) => args[0];
+export const getActiveInsuranceForDos = (...args: unknown[]) => args[0];
+export const getPrimaryInsurance = (...args: unknown[]) => args[0];
+export const getSecondaryInsurance = (...args: unknown[]) => args[0];
