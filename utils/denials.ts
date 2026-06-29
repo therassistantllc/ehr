08e73beab975839f19ba9ab0469365b0da9383fe
therassistantllc <admin@therassistant.com>
2026-06-29 11:15:03 -0600

    Update utilities file

--- utils/denials.ts
+++ utils/denials.ts
@@ -2,3 +2,5 @@ export const normalizeCarcCode = (...args: unknown[]) => args[0];
 export const normalizeRarcCode = (...args: unknown[]) => args[0];
 export const getCarcDescription = (...args: unknown[]) => args[0];
 export const getRarcDescription = (...args: unknown[]) => args[0];
+export const mapCarcToDenialCategory = (...args: unknown[]) => args[0];
+export const determineDenialWorkability = (...args: unknown[]) => args[0];
