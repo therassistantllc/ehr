2be439845fe93f403d2bbca45c0b211ddfae918d
therassistantllc <admin@therassistant.com>
2026-06-29 11:17:42 -0600

    Update utilities file

--- utils/reporting.ts
+++ utils/reporting.ts
@@ -1,1 +1,4 @@
-export {};
+export const formatReportDateRange = (...args: unknown[]) => args[0];
+export const groupByPayer = (...args: unknown[]) => args[0];
+export const groupByProvider = (...args: unknown[]) => args[0];
+export const groupByCpt = (...args: unknown[]) => args[0];
