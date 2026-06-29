4956869db52d47ad2de5d44582acc2078900b3c0
therassistantllc <admin@therassistant.com>
2026-06-29 11:19:33 -0600

    Add notification utilities

---
+++ utils/notifications.ts
@@ -0,0 +1,7 @@
+export const buildNotificationPayload = (...args: unknown[]) => args[0];
+export const formatNotificationType = (...args: unknown[]) => args[0];
+export const shouldSendNotification = (...args: unknown[]) => args[0];
+export const getNotificationRecipients = (...args: unknown[]) => args[0];
+export const buildNotificationMessage = (...args: unknown[]) => args[0];
+export const dedupeNotification = (...args: unknown[]) => args[0];
+export const formatNotificationTimestamp = (...args: unknown[]) => args[0];
