83c5656920dd46d11ee89126b096d9eeea907fa9
therassistantllc <admin@therassistant.com>
2026-06-29 11:21:49 -0600

    Add error utilities

---
+++ utils/errors.ts
@@ -0,0 +1,8 @@
+export const createAppError = (...args: unknown[]) => args[0];
+export const parseApiError = (...args: unknown[]) => args[0];
+export const getErrorMessage = (...args: unknown[]) => args[0];
+export const isRetryableError = (...args: unknown[]) => args[0];
+export const logClientError = (...args: unknown[]) => args[0];
+export const logServerError = (...args: unknown[]) => args[0];
+export const redactErrorMetadata = (...args: unknown[]) => args[0];
+export const buildErrorContext = (...args: unknown[]) => args[0];
