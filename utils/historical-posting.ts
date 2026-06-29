05a571cfc2e50c039b73b9791ed7e5baa74e918f
therassistantllc <admin@therassistant.com>
2026-06-29 11:13:13 -0600

    Add historical posting utilities

---
+++ utils/historical-posting.ts
@@ -0,0 +1,8 @@
+export const formatHistoricalTransactionType = (...args: unknown[]) => args[0];
+export const validateHistoricalTransaction = (...args: unknown[]) => args[0];
+export const buildHistoricalLedgerTransaction = (...args: unknown[]) => args[0];
+export const canLinkHistoricalTransaction = (...args: unknown[]) => args[0];
+export const buildHistoricalLinkPayload = (...args: unknown[]) => args[0];
+export const buildHistoricalReversalPayload = (...args: unknown[]) => args[0];
+export const mapLegacyTransactionType = (...args: unknown[]) => args[0];
+export const normalizeLegacySourceName = (...args: unknown[]) => args[0];
